5a10fe2a21486970f7085490d2fe5e561536c6c2
Pedro Henrique <pedrohenriquefp_2013@hotmail.com>
2017-11-23 16:41:01 -0200

    Configuring libraries in build.gradle

--- app/src/main/java/com/example/pedrohenrique/wefind/view/home/MainActivity.kt
+++ app/src/main/java/com/example/pedrohenrique/wefind/view/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pedrohenrique.wefind
+package com.example.pedrohenrique.wefind.view.home
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pedrohenrique.wefind.R
 
 class MainActivity : AppCompatActivity() {
 
